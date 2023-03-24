20ba063cf9c6f958d97d944deaf71541be5192b0
vinzen_11_2 <v@farbexot.de>
2023-03-24 16:26:28 +0100

    init windows

---
+++ windows/sandbox2023/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::sandbox2023::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::sandbox2023::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
