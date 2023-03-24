19bff0281bd41d951dfe90ad5629e96ec9b66b34
vinzen_11_2 <v@farbexot.de>
2023-03-24 18:25:51 +0100

    added document path

--- App.tsx
+++ App.tsx
@@ -1,57 +1,43 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, Text} from 'react-native';
 
-import { WebView } from 'react-native-webview';
+import {WebView} from 'react-native-webview';
 import Server from '@dr.pogodin/react-native-static-server';
 
+import RNFS from 'react-native-fs';
 
 function App(): JSX.Element {
+  React.useEffect(() => {
+    const server = new Server({
+      // See further in the docs how to statically bundle assets into the App,
+      // alternatively assets to server might be created or downloaded during
+      // the app's runtime.
+      fileDir: RNFS.DocumentDirectoryPath + '\\web\\',
+    });
 
+    server
+      .start()
+      .then(origin => {
+        console.log(`Serving at URL ${origin}`);
+      })
+      .catch(e => {
+        console.log('ERROR SERVER', e);
+      });
+
+    return () => server.stop();
+  }, []);
 
   React.useEffect(() => {
-
-
-
- const server = new Server({
-  // See further in the docs how to statically bundle assets into the App,
-  // alternatively assets to server might be created or downloaded during
-  // the app's runtime.
-  fileDir: '/path/to/static/assets/on/target/device',
-});
-
-
-server.start().then((origin) => {
-  console.log(`Serving at URL ${origin}`);
-});
-
-    return () => server.stop()
-  }, [])
-
-
+    console.log('HELLO', RNFS.MainBundlePath);
+  }, []);
 
   return (
     <SafeAreaView style={{flex: 1}}>
-     <Text> Testr </Text>
+      <Text> Testr </Text>
 
-     <WebView source={{ uri: "https://www.whatsmybrowser.org/" }} useWebView2 />
+      <WebView source={{uri: 'https://www.whatsmybrowser.org/'}} useWebView2 />
     </SafeAreaView>
   );
 }
-
 
 export default App;
